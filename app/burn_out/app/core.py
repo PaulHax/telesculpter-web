f9ec917449dce847ceb956a3b864078a85819755
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-03-07 12:00:53 -0500

    extract name of selected file

--- app/burn_out/app/core.py
+++ app/burn_out/app/core.py
@@ -125,7 +125,7 @@ class BurnOutApp:
             file_to_load = filedialog.askopenfile(
                 title="Select video to load",
             )
-            file_to_load = file_to_load.read()
+            file_to_load = file_to_load.name
             # TODO not sure how to get string from TExtIO object from above
             # file_to_load = webview_file_dialog()
 
