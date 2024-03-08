247e659c313014f65fd2ea585a9f985fe523b35d
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-03-08 07:47:56 -0800

    fix(ui): update video adaptor size when source changes

--- app/burn_out/app/utils.py
+++ app/burn_out/app/utils.py
@@ -8,12 +8,15 @@ class VideoAdapter:
         self.streamer = stream_manager
 
     def update_frame(self, kwiver_image):
-        if self.meta is None:
-            self.meta = dict(
-                type="image/rgb24",
-                w=int(kwiver_image.width()),
-                h=int(kwiver_image.height()),
-            )
+        # if we open a new video this needs to be updated. force update every time for now
+        # if self.meta is None:
+        meta = dict(
+            type="image/rgb24",
+            w=int(kwiver_image.width()),
+            h=int(kwiver_image.height()),
+        )
+        if meta != self.meta:
+            self.meta = meta
         self.streamer.push_content(
             self.area_name, self.meta, memoryview(kwiver_image.asarray().ravel())
         )
