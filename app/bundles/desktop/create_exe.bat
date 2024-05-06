6d6ae9011d80bca30506818f6dc740161620d61d
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-05-06 18:53:59 -0400

    fix(packaging): Add pyinstaller hook for kwiver-specific options

--- app/bundles/desktop/create_exe.bat
+++ app/bundles/desktop/create_exe.bat
@@ -1,13 +1,11 @@
 python -m PyInstaller ^
   --windowed ^
   --hidden-import pkgutil ^
-  --hidden-import kwiver.vital.plugins ^
-  --hidden-import kwiver.vital.plugins.discovery ^
   --hidden-import numpy ^
   --hidden-import pkgutil ^
   --collect-data trame_quasar ^
   --collect-data trame_rca ^
   --collect-data trame_client ^
   --collect-data burn_out ^
-  --collect-data kwiver ^
+  --additional-hooks-dir="." ^
   .\burn-out.py
--- app/bundles/desktop/create_linux.sh
+++ app/bundles/desktop/create_linux.sh
@@ -4,12 +4,11 @@ CURRENT_DIR=`dirname "$0"`
 
 python -m PyInstaller \
     --clean --noconfirm \
-    --hidden-import kwiver.vital.plugins \
     --hidden-import numpy \
     --hidden-import pkgutil \
     --collect-data trame_quasar \
     --collect-data trame_rca \
     --collect-data trame_client \
     --collect-data burn_out \
-    --collect-data kwiver \
+    --additional-hooks-dir="$CURRENT_DIR" \
     "$CURRENT_DIR/burn-out.py"
