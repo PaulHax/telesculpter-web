85cee39177efa256243487730755434daea8bcc0
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-05-16 05:30:10 -0700

    fix(windows): redirect stdout/stderr
    when pyinstaller is used on windows with the --windowed option, accessing stdout/err leads to
    
    AttributeError: 'NoneType' object has no attribute 'flush'.
    
    The access happends internally in trame_server but it could be more.
    We apply the suggestion of [1] from the pyinstaller documentation.
    [1] https://pyinstaller.org/en/latest/common-issues-and-pitfalls.html#sys-stdin-sys-stdout-and-sys-stderr-in-noconsole-windowed-applications-windows-only

--- app/bundles/desktop/burn-out.py
+++ app/bundles/desktop/burn-out.py
@@ -1,6 +1,15 @@
 import multiprocessing
+import os
+import sys
 
 from burn_out.app.main import app
+
+# redirect stdout/stderr to avoid issues when running on windows without a console
+# see https://pyinstaller.org/en/latest/common-issues-and-pitfalls.html#sys-stdin-sys-stdout-and-sys-stderr-in-noconsole-windowed-applications-windows-only
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, "w")
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, "w")
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
